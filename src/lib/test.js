0e50ca9528b74d0d172eb74f3f7d630cc986f2f3
jokerety <avatar51@mail.ru>
2018-10-07 15:02:41 +0300

    v1

---
+++ src/lib/test.js
@@ -0,0 +1,9 @@
+const say = function (name) {
+        var box = document.createElement('box');
+        box.className = "alert"
+	    box.innerHTML = `Hello, ${name}!`;
+        document.body.appendChild(box);
+	};
+
+
+export default say;
