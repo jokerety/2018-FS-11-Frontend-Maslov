de6fe62fc92022b76d6b9e78654fb7315184f309
jokerety <avatar51@mail.ru>
2018-10-10 14:22:58 +0300

    changes

--- src/lib/test.js
+++ src/lib/test.js
@@ -1,6 +1,6 @@
 const say = function (name) {
         var box = document.createElement('box');
-        box.className = "alert"
+        box.className = 'alert'
 	    box.innerHTML = `Hello, ${name}!`;
         document.body.appendChild(box);
 	};
