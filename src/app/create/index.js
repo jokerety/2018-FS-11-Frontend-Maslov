843026520fe1e253031465a090290a939840978c
jokerety <avatar51@mail.ru>
2018-10-31 01:22:37 +0300

    add styles + file + geo

--- src/app/create/index.js
+++ src/app/create/index.js
@@ -1,2 +1,4 @@
 import FormInput from '../../lib/components/form/-input';
 import MessageForm from '../../lib/components/message-form';
+import FormFile from '../../lib/components/form/file'
+import FormGeo from '../../lib/components/form/geo'
--- src/lib/components/message-form/index.js
+++ src/lib/components/message-form/index.js
@@ -1,21 +1,21 @@
 // import styles from './index.css';
-import shadowStyles from './shadow.css';
+import shadowStyles from './shadow.css'
 
 const slotName = 'message-input';
 
 const template = `
 	<style>${shadowStyles.toString()}</style>
-	<form>
-		<div class="result"></div>
-		<form-input name="firstname_text" placeholder="Введите имя" slot="message-input">
-			<span slot="icon"></span>
-		</form-input>
-		<form-input name="secondname_text" placeholder="Введите фамилию" slot="message-input">
-			<span slot="icon"></span>
-		</form-input>
-		<form-input name="phonenumber_text" placeholder="Введите номер телефона" slot="message-input">
-			<span slot="icon"></span>
-		</form-input>
+	<header>FORM</header>
+	<form id="form" class="topBefore">
+        <label> In local storage info:</label>
+        <div class="info">
+        </div>
+		<input name = 'name' type="text" placeholder="NAME" class="info">
+		<input name = 'surname' type="text "placeholder="SURNAME" class="info">
+		<input name = 'phone' type = "text" placeholder="PHONE NUMBER" class="info" >
+		<input name = "submit" type = "submit" value="Submit">
+		<form-geo></form-geo>
+        <form-file></form-file>
 	</form>
 `;
 
@@ -41,7 +41,9 @@ class MessageForm extends HTMLElement {
 
   _initElements() {
     const form = this.shadowRoot.querySelector('form');
-    const message = this.shadowRoot.querySelector('.result');
+    const message = this.shadowRoot.querySelector('.info');
+    message.innerText = localStorage.getItem('info');
+    localStorage.clear();
     this._elements = {
       form,
       message,
@@ -51,16 +53,15 @@ class MessageForm extends HTMLElement {
   _addHandlers() {
     this._elements.form.addEventListener('submit', this._onSubmit.bind(this));
     this._elements.form.addEventListener('keypress', this._onKeyPress.bind(this));
-    // this._elements.inputSlot.addEventListener('slotchange', this._onSlotChange.bind(this));
   }
 
   _onSubmit(event) {
-    this._elements.message.innerText = Array.from(this._elements.form.elements).map(
-      el => el.value,
-    ).join(', ');
+    this._elements.message.innerText = Array.from(this._elements.form.getElementsByClassName('info')).map(elem => elem.value).join(', ');
+    localStorage.setItem('info', this._elements.message.innerText);
     event.preventDefault();
     return false;
   }
+
 
   _onKeyPress(event) {
     if (event.keyCode == 13) {
@@ -68,5 +69,6 @@ class MessageForm extends HTMLElement {
     }
   }
 }
+
 
 customElements.define('message-form', MessageForm);
