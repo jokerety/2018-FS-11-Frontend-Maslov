81feb4748d0e1b60a8c21ae77197d9f19fbb0899
jokerety <avatar51@mail.ru>
2018-11-18 19:58:28 +0300

    new rep

--- src/app/create/index.js
+++
@@ -1,4 +0,0 @@
-import FormInput from '../../lib/components/form/-input';
-import MessageForm from '../../lib/components/message-form';
-import FormFile from '../../lib/components/form/file'
-import FormGeo from '../../lib/components/form/geo'
